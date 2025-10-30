7dcd7124a3881e6a25e7403c12388d9f3f74ac9b
Дмитрий Денисов <gustavs6669@mail.ru>
2025-10-30 19:33:49 +0200

    pyproject

---
+++ prod/mysite/shopapp/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+
+from shopapp.models import Product
+
+
+class MultipleFileInput(forms.ClearableFileInput):
+    allow_multiple_selected = True
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = "name", "price", "description", "discount", "preview"
+
+    images = forms.ImageField(
+        widget=MultipleFileInput(attrs={"multiple": True}),
+    )
+
+
+class CSVImportForm(forms.Form):
+    csv_file = forms.FileField()
